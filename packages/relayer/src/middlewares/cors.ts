feb441aa8655b71b9d958d069ffcf7735a596dec
halcyonl <halcyonl@users.noreply.github.com>
2024-03-17 08:51:43 +0700

    add cors

--- packages/relayer/src/middlewares/cors.ts
+++ packages/relayer/src/middlewares/cors.ts
@@ -2,7 +2,7 @@ import cors from "@koa/cors";
 
 export const corsMiddleware = cors({
   origin: (ctx): string => {
-    const validDomains = ["http://localhost:5173", "http://127.0.0.1:8080", "https://moon42.run"];
+    const validDomains = ["http://localhost:5173", "http://127.0.0.1:8080", "https://moon42.run", "https://linea.moon42.run"];
     if (validDomains.indexOf(ctx.request.header.origin!) !== -1) {
       return ctx.request.header.origin || "";
     }
